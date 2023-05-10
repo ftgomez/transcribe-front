3db15f45d01133be91776f5c65b0fd32b4f31e6e
Tomas Gomez <tgomez2342@gmail.com>
2023-05-10 13:07:33 -0400

    primera version funcional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Dropzone from "./components/Dropzone";
 import "./App.css"; 
 
 const App = () => {
   const [transcript, setTranscript] = useState("");
   const [userQuestion, setUserQuestion] = useState("");
-  const [messages, setMessages] = useState([]);
-  const [uploadedFile, setUploadedFile] = useState(null);
+  const [messages, setMessages] = useState([
+    {
+      sender: 'bot',
+      content: 'Hola! Bienvenido a AudioGPT. Carga un archivo MP3 y hazme preguntas sobre su contenido.',
+    },
+  ]);
+  const [chatId, setChatId] = useState("");
+  const [showTranscript, setShowTranscript] = useState(false);
+  const chatContainerRef = useRef(null);
 
-
+  const toggleTranscript = () => {
+    setShowTranscript(!showTranscript);
+  };
+  
   const handleFileUpload = async (files) => {
     const file = files[0];
   
@@ -31,6 +41,8 @@ const App = () => {
         console.error("Error moviendo el archivo:", error);
       }
 
+      addMessage("bot", "Cargando transcripción...")
+
       const query = {path: `data/${file.path}`, query: ""}
 
       const audio = await fetch("http://localhost:8000/create_chat", {
@@ -44,7 +56,11 @@ const App = () => {
 
     if (audio.ok) {
       const result = await audio.json();
-      console.log("Result:", result);
+      console.log("Chat ID:", result.chat_id);
+      setTranscript(result.transcript)
+      setChatId(result.chat_id)
+      addMessage("bot", "Audio listo. Hazme una pregunta")
+
     } else {
       console.error("Error:", audio.statusText);
     }
@@ -57,8 +73,27 @@ const App = () => {
   const handleQuestionSubmit = async (e) => {
     e.preventDefault();
     addMessage("user", userQuestion);
-    addMessage("bot", "This is a generic answer.");
     setUserQuestion("");
+    const query = {query: userQuestion}
+
+      const audio = await fetch(`http://localhost:8000/chat/${chatId}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(query)
+      }
+    )
+
+    if (audio.ok) {
+      const result = await audio.json();
+      console.log(result.response);
+      addMessage("bot", result.response);
+
+    } else {
+      console.error("Error:", audio.statusText);
+    }
+
   };
 
   const addMessage = (sender, content) => {
@@ -71,15 +106,30 @@ const App = () => {
     ]);
   };
 
+  const scrollToBottom = () => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+
   return (
     <div className="App">
       <h1>AudioGPT</h1>
       <Dropzone onDrop={handleFileUpload} />
       <div>
-        <h2>Transcripción</h2>
-        <pre>{transcript}</pre>
-      </div>
-      <div className="chat-container">
+      <button type="submit" onClick={toggleTranscript} style={{ marginTop: '20px' }}>
+        {showTranscript ? "Ocultar Transcripción" : "Mostrar Transcripción"}
+      </button>
+      {showTranscript && (
+        <div className="transcript-container">{transcript}</div>
+      )}
+    </div>
+      <div className="chat-container" ref={chatContainerRef}>
         <ul className="chat-messages">
           {messages.map((message, index) => (
             <li key={index} className={message.sender === "user" ? "user-message" : "bot-message"}>
